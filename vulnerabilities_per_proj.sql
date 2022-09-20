865652ac9be43cb202cfabfb1c7d8be68548a613
Alex <strashila@gmail.com>
2022-09-20 10:20:44 +0300

    Update vulnerabilities_per_proj.sql

--- vulnerabilities_per_proj.sql
+++ vulnerabilities_per_proj.sql
@@ -1,24 +1,23 @@
 /* Get all vulnerabilities in all the project version BOMs with component names */
 
-set search_path=reporting;  
+SET search_path=reporting;  
 
-select distinct  
-proj.project_name as "Project Name",  
-pv.version_name as "Version Name",  
-comp.component_name as "Component Name",  
-cv.vuln_id as "Vulnerability ID",  
-cv.severity as "Severity CVSS v2",  
-cv.base_score as "Base CVSS v2",  
-cv.exploit_score as "Exploit CVSS v2",  
-cv.impact_score as "Impact CVSS v2",  
-cv.temporal_score as "Overall (Temporal) CVSS v2",  
-cv.severity_cvss3 as "Severity CVSS v3.x",  
-cv.base_score_cvss3 as "Base CVSS v3.x",  
-cv.exploit_score_cvss3 as "Exploit CVSS v3.x",  
-cv.impact_score_cvss3 as "Impact CVSS v3.x",  
-cv.temporal_score_cvss3 as "Overall (Temporal) CVSS v3.x"  
-from component_vulnerability cv  
-inner join project_version pv on cv.project_version_id = pv.version_id  
-inner join project proj on proj.project_id = pv.project_id  
-inner join component comp on comp.id = cv.component_table_id  
-order by proj.project_name, pv.version_name, comp.component_name, cv.vuln_id;  
+SELECT DISTINCT proj.project_name AS "Project Name",  
+                pv.version_name AS "Version Name",  
+                comp.component_name AS "Component Name",  
+                cv.vuln_id AS "Vulnerability ID",  
+                cv.severity AS "Severity CVSS v2",  
+                cv.base_score AS "Base CVSS v2",  
+                cv.exploit_score AS "Exploit CVSS v2",  
+                cv.impact_score AS "Impact CVSS v2",  
+                cv.temporal_score AS "Overall (Temporal) CVSS v2",  
+                cv.severity_cvss3 AS "Severity CVSS v3.x",  
+                cv.base_score_cvss3 AS "Base CVSS v3.x",  
+                cv.exploit_score_cvss3 AS "Exploit CVSS v3.x",  
+                cv.impact_score_cvss3 AS "Impact CVSS v3.x",  
+                cv.temporal_score_cvss3 AS "Overall (Temporal) CVSS v3.x"  
+FROM component_vulnerability cv  
+INNER JOIN project_version pv ON cv.project_version_id = pv.version_id  
+INNER JOIN project proj ON proj.project_id = pv.project_id  
+INNER JOIN component comp ON comp.id = cv.component_table_id  
+ORDER BY proj.project_name, pv.version_name, comp.component_name, cv.vuln_id;  
