c0651dc4f41a69399ece6bc09c6faf0cb21673f0
Alex <strashila@gmail.com>
2022-09-20 10:17:50 +0300

    Update component_projects.sql

--- component_projects.sql
+++ component_projects.sql
@@ -1,10 +1,13 @@
 /* Get all projects per component name, for example Log4j */   
 
+SET search_path=reporting;  
 
-set search_path=reporting;  
-
-select distinct comp.component_name, proj.project_name, pv.version_name  
-from component comp inner join project_version pv on comp.project_version_id = pv.version_id  
-inner join project proj on proj.project_id = pv.project_id  
-where comp.component_name like '%Log4j%'  
-order by comp.component_name, proj.project_name, pv.version_name  
+SELECT DISTINCT comp.component_name, 
+                proj.project_name, 
+                pv.version_name  
+                
+FROM component comp
+INNER JOIN project_version pv ON comp.project_version_id = pv.version_id  
+INNER JOIN project proj ON proj.project_id = pv.project_id  
+WHERE comp.component_name LIKE '%Log4j%'  
+ORDER BY comp.component_name, proj.project_name, pv.version_name  
