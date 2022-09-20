62622523a5ea51140c35ff908838081450e98533
Alex <strashila@gmail.com>
2022-09-20 10:18:18 +0300

    Update component_projects.sql

--- component_projects.sql
+++ component_projects.sql
@@ -4,8 +4,7 @@ SET search_path=reporting;
 
 SELECT DISTINCT comp.component_name, 
                 proj.project_name, 
-                pv.version_name  
-                
+                pv.version_name      
 FROM component comp
 INNER JOIN project_version pv ON comp.project_version_id = pv.version_id  
 INNER JOIN project proj ON proj.project_id = pv.project_id  
