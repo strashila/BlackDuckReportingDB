091bda3321e9761ff347c9b174a19179ffbe62bd
Alexander Knurenko <strashila@gmail.com>
2022-11-28 10:16:51 +0200

    Update active_users_last_login.sql

--- active_users_last_login.sql
+++ active_users_last_login.sql
@@ -3,3 +3,10 @@
 SELECT * FROM reporting.user  
 WHERE last_login > CURRENT_DATE - INTERVAL '6 months'  
   AND active = true;
+  
+  
+/* Alternatively, users that did NOT log in in the last 6 months */
+
+SELECT * FROM reporting.user
+WHERE last_login < CURRENT_DATE - INTERVAL '180 days'
+ORDER BY last_login DESC
